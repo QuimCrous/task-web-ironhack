a11c04054cde5f533e6384e9c2a347ae721ded47
Quim Crous <joaquimcrous@gmail.com>
2023-02-24 13:44:40 +0100

    small fix 2

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,6 +1,6 @@
 <template>
   <Nav />
-  <div class="flex justify-center items-center mt-10">
+  <div class="flex justify-center items-center mt-10 mb-20">
     <div
       class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 shadow-2xl"
     >
